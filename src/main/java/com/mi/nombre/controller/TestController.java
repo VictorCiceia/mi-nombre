f382faafa3a3415c5db359a2c3044d589da7d042
VictorCiceia <victormciceia@gmail.com>
2024-06-09 21:22:49 -0400

    Endpoint Hola

---
+++ src/main/java/com/mi/nombre/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.mi.nombre.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/hola/{nombre}")
+    public String sayHello(@PathVariable String nombre) {
+        return "Hola, " + nombre + "!";
+    }
+}
